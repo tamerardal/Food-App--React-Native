acf1d3dae8a7be4a8c7c12560cd82461bc5b7e3a
Tamer <47778933+tamerardal@users.noreply.github.com>
2022-10-17 15:48:37 +0300

    search bar olusturuldu.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,41 @@
 import React from "react";
 import {View, TextInput, StyleSheet, Text} from 'react-native';
 
-const SearchBar = () => {
+import { Ionicons } from "@expo/vector-icons";
+
+
+const SearchBar = ({term, onTermChange}) => {
     return (
         <View style={styles.container}>
-            <Text>Search Bar</Text>
+            <Ionicons name="search" size={30} style={styles.icon} />
+            <TextInput 
+                placeholder="Search" 
+                style={styles.input} 
+                value={term} 
+                onChangeText={newTerm => onTermChange(newTerm)}
+                autoCorrect={false} 
+                autoCapitalize="none" />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#F0EEEE',
+        marginTop: 10,
+        backgroundColor: 'lightgrey',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
+        flexDirection: 'row',
     },
+    input: {
+        borderColor: 'black',
+        flex: 1,
+        borderRadius: 20
+    },
+    icon: {
+        margin: 8,
+    }
 });
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 
+
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    console.log(term);
     return (
         <SafeAreaView style = {styles.container}>
             <View>
-                <SearchBar />
+                <SearchBar term={term} onTermChange = {newTerm => setTerm(newTerm)} />
             </View>
         </SafeAreaView>
     );
